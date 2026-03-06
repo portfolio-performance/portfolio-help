3bf8192e86ed7596506188c2f3ea98003323844d
hug-sch <hug-sch@outlook.com>
2026-03-06 10:24:48 +0100

    update serve.sh for livereload on safari

--- serve.sh
+++ serve.sh
@@ -4,4 +4,4 @@
 cd config/en || { echo "Directory config/en not found"; exit 1; }
 
 # Activate the pipx environment for mkdocs (assuming it's named mkdocs)
-mkdocs serve
+mkdocs serve --dirtyreload --livereload
